d393da33426d845f85861ca6ec6fbcb8aab090a1
Marko Dujović <dujovicm@outlook.com>
2023-07-25 18:22:40 +0200

    Authentication service

---
+++ Services/AuthenticationService.cs
@@ -0,0 +1,59 @@
+﻿using Blazored.LocalStorage;
+using ClientWebApp.Auth;
+using ClientWebApp.AuthProviders;
+using ClientWebApp.Features;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+
+namespace ClientWebApp.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient client;
+        private readonly JsonSerializerOptions options;
+        private readonly AuthenticationStateProvider authProvider;
+        private readonly ILocalStorageService localStorage;
+
+        public AuthenticationService(HttpClient client, AuthenticationStateProvider authProvider, ILocalStorageService localStorage)
+        {
+            this.client = client;
+            this.authProvider = authProvider;
+            this.localStorage = localStorage;
+            this.options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public async Task<AuthResponse> Login(UserAuth userAuth)
+        {
+            var content = JsonSerializer.Serialize(userAuth);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+
+            var authResult = await client.PostAsync("api/users/authenticate", bodyContent);
+            var authContent = await authResult.Content.ReadAsStringAsync();
+
+            var result = JsonSerializer.Deserialize<AuthResponse>(authContent, options);
+
+            if (!authResult.IsSuccessStatusCode)
+            {
+                return result;
+            }
+
+            await localStorage.SetItemAsync("token", result.Token);
+
+            ((AuthStateProvider) authProvider).NotifyUserAuthentication(userAuth.Username);
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.Token);
+
+            return new AuthResponse { IsAuthSuccessful = true };
+        }
+
+        public async Task Logout()
+        {
+            await localStorage.RemoveItemAsync("token");
+            ((AuthStateProvider) authProvider).NotifyUserLogout();
+            client.DefaultRequestHeaders.Authorization = null;
+        }
+    }
+}
---
+++ Services/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using ClientWebApp.Auth;
+
+namespace ClientWebApp.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthResponse> Login(UserAuth userAuth);
+        Task Logout();
+    }
+}
