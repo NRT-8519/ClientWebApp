3384b358bbe2371049fd2351916bf7d9209bd1f7
Marko Dujović <dujovicm@outlook.com>
2023-07-25 18:23:54 +0200

    Frontend Blazor components

---
+++ Pages/Login.razor.cs
@@ -0,0 +1,36 @@
+﻿using ClientWebApp.Auth;
+using ClientWebApp.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace ClientWebApp.Pages
+{
+    public partial class Login
+    {
+        private UserAuth userAuth = new UserAuth();
+
+        [Inject]
+        public IAuthenticationService AuthenticationService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public bool ShowAuthError {  get; set; }
+        public string Error { get; set; }
+
+        public async Task ExecuteLogin()
+        {
+            ShowAuthError = false;
+
+            var result = await AuthenticationService.Login(userAuth);
+            if (!result.IsAuthSuccessful)
+            {
+                Error = result.ErrorMessage;
+                ShowAuthError = true;
+            }
+            else
+            {
+                NavigationManager.NavigateTo("/");
+            }
+        }
+    }
+}
