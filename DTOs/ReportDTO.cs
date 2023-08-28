879331d20a8b4104393af41920820a2f5cb8224c
Marko Dujović <dujovicm@outlook.com>
2023-08-28 13:25:09 +0200

    Reports (WIP)

---
+++ DTOs/ReportDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ClientWebApp.DTOs
+{
+    public class ReportDTO
+    {
+        public Guid ReportedBy { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
