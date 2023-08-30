cd810033ccc744a4390fa00114d6a36742904085
Marko Dujović <dujovicm@outlook.com>
2023-08-30 15:48:51 +0200

    Annotation Validation

--- Auth/UserAuth.cs
+++ Auth/UserAuth.cs
@@ -1,8 +1,17 @@
-﻿namespace ClientWebApp.Auth
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ClientWebApp.Auth
 {
     public class UserAuth
     {
+        [Required(ErrorMessage = "Please type in a username")]
+        [StringLength(30)]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please type in a password")]
+        [StringLength(45)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
