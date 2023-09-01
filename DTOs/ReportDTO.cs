51c0c51713db999128f2b9d329c074bc56df24f8
Marko Dujović <dujovicm@outlook.com>
2023-09-01 12:00:46 +0200

    Reports and requests

--- DTOs/ReportDTO.cs
+++ DTOs/ReportDTO.cs
@@ -2,8 +2,10 @@
 {
     public class ReportDTO
     {
+        public Guid UUID { get; set; }
         public Guid ReportedBy { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public DateTime Date { get; set; }
     }
 }
